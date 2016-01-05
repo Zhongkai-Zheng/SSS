15b8558d620a6eef52ecb390dff62fc829ed83b8
dniu16 <dniu16@choate.edu>
2016-01-05 09:28:13 -0500

    Jeff

---
+++ client/templates/Register.js
@@ -0,0 +1,15 @@
+Template.register.events({
+		'submit form': function(event){
+			event.preventDefault();
+			var email = $('[name=email]').val();
+			var password = $('[name=password]').val();
+
+			Accounts.createUser({email: email, password: password}, function(error){
+				if(error) {
+					console.log(error.reason); 
+				} else {
+					 Router.go("main"); 
+				}
+			});
+		}
+});
