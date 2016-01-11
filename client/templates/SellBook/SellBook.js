d8388dbdca10f17022d9248e985095f5425043c1
Ian Wolterstorff <ianwolt@yahoo.com>
2016-01-11 00:12:35 -0500

    Began adding CollectionFS for image storage and fixed routing bug

---
+++ client/templates/SellBook/SellBook.js
@@ -0,0 +1,28 @@
+Template.SellBook.events({
+	'submit form': function(event, template){
+		event.preventDefault();
+
+		if (Meteor.user() != null) {
+			var user = Meteor.user();
+
+			var fileInput = event.target.fileInput;
+			var files = fileInput.files;
+
+			var title = event.target.title;
+			var firstname = event.target.firstname;
+			var lastname = event.target.lastname;
+
+			for (var i = 0, ln = files.length; i < ln; i++) {
+				var newFile = new FS.File(files[i]);
+
+				newFile.username = user.username;
+				newFile.userId = user._id;
+				newFile.title = title;
+				newFile.firstname = firstname;
+				newFile.lastname = lastname;
+			}
+
+			// TODO: Create GridFS collection and add that here
+		}
+	}
+})
