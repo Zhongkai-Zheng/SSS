51bb98f9a306cfee435bd89914214d99a5da8f42
dniu16 <dniu16@choate.edu>
2016-01-14 15:13:41 -0500

    Sellbook css

--- client/templates/common.js
+++ client/templates/common.js
@@ -1,4 +1,4 @@
-UI.registerHelper('bounceLoggedOut', function() {
+UI.registerHelper('startscreen', function() {
 	console.log(Meteor.userId());
 	if (Meteor.userId() == null) {
 		Router.go('login');
