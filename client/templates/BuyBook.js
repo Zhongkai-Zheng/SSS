c719620be94b1b582efd68f22505ad08f6f001f6
dniu16 <dniu16@choate.edu>
2016-01-27 11:32:17 -0500

    working on search bar

--- client/templates/BuyBook.js
+++ client/templates/BuyBook.js
@@ -20,7 +20,7 @@ Template.buybook.helpers({
  	'images': function() {
     return Images.find();
 
-  }
+  },
 });
 
   Template.buybook.events({
@@ -62,3 +62,36 @@ var incrementLimit = function(templateInstance) {
     parseInt(Meteor.settings.public.recordsPerPage);
   templateInstance.limit.set(newLimit);
 }
+var options = {
+  keepHistory: 1000 * 60 * 5,
+  localSearch: true
+};
+var fields = ['packageName', 'description'];
+
+PackageSearch = new SearchSource('packages', fields, options);
+
+Template.searchResult.helpers({
+  getPackages: function() {
+    return PackageSearch.getData({
+      transform: function(matchText, regExp) {
+        return matchText.replace(regExp, "<b>$&</b>")
+      },
+      sort: {isoScore: -1}
+    });
+  },
+  
+  isLoading: function() {
+    return PackageSearch.getStatus().loading;
+  }
+});
+
+Template.searchResult.rendered = function() {
+  PackageSearch.search('');
+};
+
+Template.searchBox.events({
+  "keyup #search-box": _.throttle(function(e) {
+    var text = $(e.target).val().trim();
+    PackageSearch.search(text);
+  }, 200)
+});
--- server/server.js
+++ server/server.js
@@ -3,3 +3,24 @@ bounceLoggedOut = function() {
 		throw new Meteor.Error('unauthorized', 'you need to be logged in for that!');
 	}
 }
+SearchSource.defineSource('packages', function(searchText, options) {
+  var options = {sort: {isoScore: -1}, limit: 20};
+  
+  if(searchText) {
+    var regExp = buildRegExp(searchText);
+    var selector = {$or: [
+      {packageName: regExp},
+      {description: regExp}
+    ]};
+    
+    return Packages.find(selector, options).fetch();
+  } else {
+    return Packages.find({}, options).fetch();
+  }
+});
+
+function buildRegExp(searchText) {
+  // this is a dumb implementation
+  var parts = searchText.trim().split(/[ \-\:]+/);
+  return new RegExp("(" + parts.join('|') + ")", "ig");
+}
