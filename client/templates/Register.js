1e8c0abe52b1e794a9273f53305437fa71dad156
Alex Zhongkai Zheng <Alex_Zheng@Zhongkai.local>
2016-03-01 19:56:58 -0500

    worked on some confirmation email, not done yet.

--- client/templates/Register.js
+++ client/templates/Register.js
@@ -12,16 +12,20 @@ Template.register.events({
 			text: 'Thank you for registering an SSS account! \n Your confirmation code: '+ confirmationCode
 		});
 
-			Router.go("buybook"); 
+			Meteor.call('sendthething', function(err, ret) {
+    			console.log(err);
+   			 	console.log('ret')
+});
 
-			Accounts.createUser({email: email, password: password}, function(error){
-				if(error) {
-					toastr.error("Username is invalid or already exists!");
-					console.log(error.reason); 
-				} else {
-					toastr.success("Register successful!")
-					Router.go("buybook"); 
-				}
-			});
+			// Accounts.createUser({email: email, password: password}, function(error){
+			// 	if(error) {
+			// 		toastr.error("Username is invalid or already exists!");
+			// 		console.log(error.reason); 
+			// 	} else {
+			// 		toastr.success("Register successful!")
+			// 		Router.go("buybook"); 
+			// 	}
+			// });
+			console.log("is going through");
 		}
 });
