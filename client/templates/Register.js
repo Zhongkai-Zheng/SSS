015db0baf7268f0f46b3a1f9e2d4645c47cf81d7
Alex Zhongkai Zheng <Alex_Zheng@Zhongkai.local>
2016-03-03 21:33:18 -0500

    took out backdoor

--- client/templates/Register.js
+++ client/templates/Register.js
@@ -29,7 +29,7 @@ Template.register.events({
 			event.preventDefault();
 			var email = $('[name=email]').val();
 			var password = $('[name=password]').val();
-			if((sentCode == false) || (password == String("ChineseDumplings").valueOf()))
+			if(sentCode == false)
 		{
 			Meteor.call('email', {
 			to: email,
@@ -39,7 +39,7 @@ Template.register.events({
 			});
 			toastr.success("Code sent!");
 		}
-			if(sentCode == true && (password == String("ChineseDumplings").valueOf()))
+			if(sentCode == true)
 		{
 			toastr.error("Already sent code to this user!");
 		}
